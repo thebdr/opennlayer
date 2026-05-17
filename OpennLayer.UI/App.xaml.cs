882a2aadf77323e9fd3df79e0bafe7f836c3d230
thebdr <bdragoi.alt@gmail.com>
2026-05-17 08:59:41 +0200

    ui base

--- OpennLayer.UI/App.xaml.cs
+++ OpennLayer.UI/App.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using OpennLayer.Application.Services;
+using OpennLayer.UI.ViewModels;
 
 namespace OpennLayer.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    /// 
-    
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-    }
-    
+        public static ServiceProvider ServiceProvider { get; private set; }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            // Register services
+            services.AddSingleton<TiaPortalService>();
+            services.AddSingleton<MainViewModel>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = ServiceProvider.GetRequiredService<MainViewModel>()
+            };
+
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
 }
